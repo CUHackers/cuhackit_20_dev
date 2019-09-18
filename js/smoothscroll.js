f03c149b8b8cf0f386a0d252415e7a80b4d1a99f
Charlie Gallentine <chgallentine@gmail.com>
2019-09-18 14:22:27 -0400

    CSS portability changes

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -2,7 +2,7 @@
 * @Author: Charlie Gallentine
 * @Date:   2019-09-10 13:34:24
 * @Last Modified by:   Charlie Gallentine
-* @Last Modified time: 2019-09-15 22:32:06
+* @Last Modified time: 2019-09-18 13:35:52
 */
 window.smoothScroll = function(target) {
     var scrollContainer = target;
@@ -15,7 +15,7 @@ window.smoothScroll = function(target) {
     var targetY = 0;
     do { //find the top of target relatively to the container
         if (target == scrollContainer) break;
-        targetY += target.offsetTop - 50;
+        targetY += target.offsetTop - 35;
     } while (target = target.offsetParent);
 
     scroll = function(c, a, b, i) {
